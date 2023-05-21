e67ec4d83c841fefd1aa16509f988d4bdc3ac369
Manish Kumar <sihagmanish36@gmail.com>
2023-05-21 00:37:09 -0500

    Updated streamlit app file

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import pandas as pd
 from glob import glob
+import pickle
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
@@ -15,38 +16,26 @@ import seaborn as sns
 
 import torch
 import torchvision.transforms as transforms
-from torchvision.models import vgg16
 from PIL import Image
-
-import warnings
-
-sns.set_style("whitegrid")
-warnings.filterwarnings("ignore")
-
-
-data = pd.read_csv("../data/data.csv")
-del data["Unnamed: 0"]
-
-img_paths, y, train_test = [], [], []
-for i in range(len(data)):
-    p = f"../data/Images/{data.name[i]}"
-    if os.path.exists(p):
-        img_paths.append(p)
-        y.append(data.bmi[i])
-        train_test.append(data.is_training[i])
-
-
-y = np.array(y)
-
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
+import warnings
+
+warnings.filterwarnings("ignore")
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
 # If required, create a face detection pipeline using MTCNN:
 mtcnn = MTCNN(
-    image_size=160, margin=0, min_face_size=20,
+    image_size=160, margin=40, min_face_size=20,
     thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+    device=device
+)
+
+mtcnn2 = MTCNN(
+    image_size=160, margin=40, min_face_size=20,
+    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=False,
     device=device
 )
 
@@ -65,14 +54,17 @@ def extract_features(img):
     face = mtcnn(img)
     if face is None:
         face = preprocess(img)
-
+    
     img = torch.stack([face]).to(device)
-
+    
     with torch.no_grad():
         features = resnet(img)
-
+    
     return features[0].cpu().numpy()
 
+
+with open("../models/lr.p", "rb") as f:
+    lr = pickle.load(f)
 
 img_file_buffer = st.camera_input("Take a picture")
 
@@ -80,5 +72,9 @@ if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
     img = Image.open(img_file_buffer)
     
+    detected_image = Image.fromarray(mtcnn2(img).numpy().transpose(1, 2, 0).astype(np.uint8))
+    st.image(detected_image, caption="Detected Face")
+    
     embeddings = extract_features(img)
-    st.write(embeddings.shape)
+    bmi = round(lr.predict([embeddings])[0], 2)
+    st.write(f"Your BMI is {bmi}")
