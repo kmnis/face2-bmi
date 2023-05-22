fabaab05aaa4d2901d07d76f6d11c2f024c71cc2
Manish Kumar <sihagmanish36@gmail.com>
2023-05-22 09:44:32 -0500

    Updated setup script to deploy app

--- setup.sh
+++ setup.sh
@@ -1,14 +1,3 @@
-# conda create -n face2bmi python=3.10
+docker build -t bmi_predictor:v1 .
 
-pip install -r requirements.txt
-
-jupyter contrib nbextension install --user
-jupyter nbextensions_configurator enable --user
-
-jupyter nbextension enable --py --sys-prefix appmode && \
-        jupyter serverextension enable --py --sys-prefix appmode && \
-        jupyter nbextension enable highlight_selected_word/main --sys-prefix && \
-        jupyter nbextension enable codefolding/main --sys-prefix && \
-        jupyter nbextension enable execute_time/ExecuteTime --sys-prefix
-
-pip install git+https://github.com/rcmalli/keras-vggface.git
+docker run -p 8501:8501 bmi_predictor:v1
